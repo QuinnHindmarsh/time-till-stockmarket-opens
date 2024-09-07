f4eba2e77ac8a3ee2ae8bc3487818621d2b38d25
QuinnHindmarsh <qhindmarsh@gmail.com>
2024-09-07 16:38:32 +0930

    continued tut

--- market_countdown_app.py
+++ market_countdown_app.py
@@ -1,4 +1,6 @@
 from kivy.app import App
+from main_widget import MainWidget
+from my_box_layout import MyBoxLayout
 
 
 class MarketCountdownApp(App):
---
+++ my_box_layout.py
@@ -0,0 +1,23 @@
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
+
+class MyBoxLayout(BoxLayout):
+    pass
+
+
+"""
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = "vertical"
+
+        b1 = Button(text='a')
+        b2 = Button(text='b')
+        b3 = Button(text='c')
+        b4 = Button(text='d')
+
+        self.add_widget(b1)
+        self.add_widget(b2)
+        self.add_widget(b3)
+        self.add_widget(b4)
+"""
